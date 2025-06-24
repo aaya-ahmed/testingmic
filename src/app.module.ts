b28fb450d2fa89800aded2b7e53291a986393e63
aaya-ahmed <aaya7098@gmail.com>
2025-06-24 15:49:36 +0300

    refactor: remove unused dependencies and code
    - Removed several unused dependencies from package.json including keycloak-angular, bootstrap, and ngx-lottie.
    - Removed NgbModule from app.module.ts as it was not being used.
    - Deleted init-localize.ts file as it was no longer necessary.
    - Updated main.ts to remove the import of init-localize.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { AppComponent } from "./app/app.component";
 import { NavBarComponent } from "./app/shared/components/nav-bar/nav-bar.component";
 import {BrowserAnimationsModule}from "@angular/platform-browser/animations"
@@ -19,7 +18,6 @@ import { AppRoutingModule } from "./app/app-routing.module";
         BrowserModule,
         HttpClientModule,
         AppRoutingModule,
-        NgbModule,
         NavBarComponent,],
     
     bootstrap: [AppComponent],
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 async function initializeApp() {
   await import('./init-zone')
-  await import('./init-localize')
   const { initializeProdMode } = await import('./init-prod-mode');
   const { initializeConsole } = await import('./init-console');
   const { bootstrapAngularApp } = await import('./init-bootstrap');
